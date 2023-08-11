6ca980c8e2566b76d9e5f8f6a345952ffc45350c
bp289 <birajpanta112@gmail.com>
2023-08-11 09:38:51 +0100

    removed changes

--- scripts/get_json.rb
+++ scripts/get_json.rb
@@ -176,6 +176,7 @@ class GetJson
     call = "git archive --remote=ssh://git@git.bioconductor.org/packages/biocViews #{branch} inst/extdata/biocViewsVocab.sqlite | tar -x --strip=2 -C #{tempDir}"
 #    local test changes to biocViews
 #    call = "cp /home/lori/b/Rpacks/biocViews/inst/extdata/biocViewsVocab.sqlite #{tempDir}"
+
     system(call)
     dbfile = "#{tempDir}/biocViewsVocab.sqlite"
     db = SQLite3::Database.new(dbfile)
